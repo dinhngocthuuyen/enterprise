e68a3cdb093aea4f02030a3b8e6b32f40b32a66b
dinhngocthuuyen <thuuyen.0621@gmail.com>
2021-02-04 20:33:39 +0700

    merge code

--- contributions-management-server/db/models/index.js
+++ contributions-management-server/db/models/index.js
@@ -1,18 +1,12 @@
 const {Faculty} = require('./faculty.model');
 const {Contribution} = require('./contribution.model')
-<<<<<<< HEAD
 const {Student} = require('./student.model')
 
-=======
 const {Coordinator} = require('./coordinator.model')
->>>>>>> 00ea7b89cf1b8c458d6e048db759c8f682741636
 
 module.exports = {
     Faculty,
     Contribution,
-<<<<<<< HEAD
     Student,
-=======
     Coordinator,
->>>>>>> 00ea7b89cf1b8c458d6e048db759c8f682741636
 }
